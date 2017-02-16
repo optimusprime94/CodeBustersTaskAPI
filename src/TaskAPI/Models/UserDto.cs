0cd73e07449e57d9cbce3e145a83b912c0b12acb
Administrator <Administrator@COMPUTER>
2017-02-16 11:44:53 +0100

    Added UserDto

---
+++ src/TaskAPI/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskAPI.Models
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
